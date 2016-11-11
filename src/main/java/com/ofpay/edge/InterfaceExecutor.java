95e54da83e2b7ddd19c00e9781e76681c965f117
aqlu <angus.aqlu@gmail.com>
2016-11-11 18:02:48 +0800

    fix fast json bug. map中key、value都为数字时，序列化的json格式错误

--- src/main/java/com/ofpay/edge/InterfaceExecutor.java
+++ src/main/java/com/ofpay/edge/InterfaceExecutor.java
@@ -3,9 +3,7 @@
  */
 package com.ofpay.edge;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,8 +49,9 @@ public class InterfaceExecutor {
             throw e;
         }
 
-        return JSON.toJSONString(result, SerializerFeature.QuoteFieldNames, SerializerFeature.WriteMapNullValue,
-                SerializerFeature.SortField);
+        return objectMapper.writeValueAsString(result);
+//        return JSON.toJSONString(result, SerializerFeature.QuoteFieldNames, SerializerFeature.WriteMapNullValue,
+//                SerializerFeature.SortField);
     }
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
